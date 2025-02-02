d8cbd10f18358b195c73005261edb10b8df02dba
Tamir Beneden <tamirbe2@ac.sce.ac.il>
2025-02-02 11:17:43 +0200

    initial commit

---
+++ integration/Trapezoidal_method.py
@@ -0,0 +1,22 @@
+import math
+
+
+def trapezoidal_rule(f, a, b, n):
+
+    h = (b - a) / n
+    T = f(a) + f(b)
+    integral = 0.5 * T  # Initialize with endpoints
+
+    for i in range(1, n):
+        x_i = a + i * h
+        integral += f(x_i)
+
+    integral *= h
+
+    return integral
+
+
+if __name__ == '__main__':
+    f = lambda x:math.e ** (x ** 2 )
+    result = trapezoidal_rule(f, 0, 1, 2)
+    print("Approximate integral:", result)
